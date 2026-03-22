d80c843ffa7e8dc581175fa4e967d6b37e7a5409
Vard <avadher510@gmail.com>
2026-03-22 20:21:31 +0000

    feat(bff): wire cart module into app

--- bff/src/app.module.ts
+++ bff/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './modules/products/products.module';
 import { DiscountsModule } from './modules/discounts/discounts.module';
+import { CartModule } from './modules/cart/cart.module';
 
 @Module({
-  imports: [ProductsModule, DiscountsModule],
+  imports: [ProductsModule, DiscountsModule, CartModule],
   controllers: [AppController],
   providers: [AppService],
 })
